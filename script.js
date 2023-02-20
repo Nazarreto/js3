f9929d69283e886eec67a37575a2b4ce40c2d907
Nazarreto <102985652+Nazarreto@users.noreply.github.com>
2023-02-20 20:57:05 +0200

    qwe

---
+++ script.js
@@ -0,0 +1,83 @@
+// task 1
+
+
+// let n = +prompt('Введіть число') 
+//     let result = 1;
+//     for (let i = 2; i <= n; i++) {
+//       result *= i;
+//     }
+//     console.log(result);
+  
+
+
+// task 2
+
+
+// for(let count =1000; count<=9999; count+=3){
+//     document.write(`<h2>Loop ${count}</h2>`)
+// }
+
+
+// task 3
+
+
+// for(let count =1; count<=99; count+=2){
+//     document.write(`<h2>Loop ${count}</h2>`)
+// }
+
+
+// task 4
+
+
+// for(let count =90; count>=0; count-=5){
+//     document.write(`<h2>Loop ${count}</h2>`)
+// }
+
+
+// task 5
+
+
+// for(let count =2; count<=1119999; count*=2){
+//     document.write(`<h2>Loop ${count}</h2>`)
+// }
+
+
+// task 6
+
+// let a = 2;
+// while (2*a-1 < 10000) {
+//   console.log(2*a-1);
+//   a++;
+// }
+
+
+// task 7
+
+// let a = -166;
+// while (2*a+200 < 100) {
+//   if (2*a+200 > 9 && 2*a+200 < 100) {
+//     console.log(2*a+200);
+//   }
+//   a++;
+// }
+
+
+// task 8 
+
+// let a = prompt("Введіть число: ");
+// let b = prompt("Введіть степінь: ");
+// let result = 1;
+
+// for (let i = 1; i <= Math.abs(b); i++) {
+//   result *= a;
+// }
+
+// if (b < 0) {
+//   result = 1 / result;
+// } else if (b === 0) {
+//   result = 1;
+// }
+
+// console.log(`${a} в степені ${b} дорівнює ${result}`);
+
+
